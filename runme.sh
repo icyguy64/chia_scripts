ee22fa3a09e8c1de80ff70e7e733fcf6ddfd2e1e
icyguy64 <54403028+icyguy64@users.noreply.github.com>
2021-05-09 10:49:30 +0800

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,11 +1,12 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt install git -y
-sudo mkfs -t ext4 /dev/nvme1n1
+sudo mkfs -t ext4 /dev/nvme2n1
 sudo mkdir /plots
 sudo mkdir /temp
+sudo mkdir /temp
 sudo mount /dev/nvme2n1 /plots/
-sudo mount /dev/nvme1n1 /temp/
+sudo mount /dev/nvme1n1 /temp2/
 sudo mount /dev/nvme0n1 /temp/
 git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
 cd chia-blockchain
