033afea512b398373f6ba13db408b3280ffe4410
icyguy64 <54403028+icyguy64@users.noreply.github.com>
2021-05-10 00:59:44 +0800

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -8,7 +8,7 @@ sudo mkfs -t ext4 /dev/nvme4n1
 sudo mkfs -t ext4 /dev/nvme5n1
 sudo mkfs -t ext4 /dev/nvme6n1
 sudo mkfs -t ext4 /dev/nvme7n1
-sudo mkfs -t ext4 /dev/nvme8n1
+
 sudo mkdir /plots
 sudo mkdir /temp
 sudo mkdir /temp2
@@ -18,15 +18,15 @@ sudo mkdir /temp5
 sudo mkdir /temp6
 sudo mkdir /temp7
 sudo mkdir /temp8
-sudo mount /dev/nvme1n1 /plots/
 sudo mount /dev/nvme0n1p1 /temp/
-sudo mount /dev/nvme2n1 /temp2/
-sudo mount /dev/nvme3n1 /temp3/
-sudo mount /dev/nvme4n1 /temp4/
-sudo mount /dev/nvme5n1 /temp5/
-sudo mount /dev/nvme6n1 /temp6/
-sudo mount /dev/nvme7n1 /temp7/
-sudo mount /dev/nvme8n1 /temp8/
+sudo mount /dev/nvme1n1 /temp2/
+sudo mount /dev/nvme2n1 /temp3/
+sudo mount /dev/nvme3n1 /temp4/
+sudo mount /dev/nvme4n1 /temp5/
+sudo mount /dev/nvme5n1 /temp6/
+sudo mount /dev/nvme6n1 /temp7/
+sudo mount /dev/nvme7n1 /temp8/
+sudo mount /dev/nvme8n1 /plots/
 git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
 cd chia-blockchain
 sh install.sh
